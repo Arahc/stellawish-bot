79ab524bd5d160c75ac02f5677712637426de348
Arahc <chenza2024@shanghaitech.edu.cn>
2026-08-24 22:23:59 +0800

    fix bug

--- source/library/cover_manager.py
+++ source/library/cover_manager.py
@@ -1,6 +1,7 @@
 import httpx
 from pathlib import Path
 from PIL import Image
+import os
 
 COVER_URL = "https://assets2.lxns.net/maimai/jacket/{id:04d}.png"
 COVER_DIR =  Path(__file__).parent.parent / "data" / "pics" / "covers"
@@ -11,7 +12,11 @@ SMALL_DIR.mkdir(parents=True, exist_ok=True)
 
 async def getCover(song_id: int) -> Image.Image:
     file_path = COVER_DIR / f"{song_id % 10000:04d}.png"
-    if not file_path.exists():
+    try:
+        return Image.open(file_path).convert("RGBA")
+    except (Image.UnidentifiedImageError, OSError):
+        if file_path.exists():
+            os.remove(file_path)
         async with httpx.AsyncClient() as client:
             try:
                 resp = await client.get(COVER_URL.format(id=song_id % 10000))
@@ -22,8 +27,7 @@ async def getCover(song_id: int) -> Image.Image:
                     raise ValueError(f"No cover for song id {song_id} found.")
             except Exception:
                 raise ValueError(f"No cover for song id {song_id} found.")
-                
-    return Image.open(file_path).convert("RGBA")
+        return Image.open(file_path).convert("RGBA")
 
 async def getSmallCover(song_id: int, size: int = 100) -> Image.Image:
     file_path = SMALL_DIR / f"{song_id % 10000:04d}.png"
