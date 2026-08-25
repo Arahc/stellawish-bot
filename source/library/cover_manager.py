1b80c482df053acda6626493c51721923c498c77
Arahc <chenza2024@shanghaitech.edu.cn>
2026-08-25 15:43:21 +0800

    back to lx

--- source/library/cover_manager.py
+++ source/library/cover_manager.py
@@ -3,19 +3,12 @@ from pathlib import Path
 from PIL import Image
 import os
 
-# COVER_URL = "https://assets2.lxns.net/maimai/jacket/{id:04d}.png"
-COVER_URL = "https://www.diving-fish.com/covers/{id:05d}.png"
+COVER_URL = "https://assets2.lxns.net/maimai/jacket/{id:04d}.png"
 COVER_DIR =  Path(__file__).parent.parent / "data" / "pics" / "covers"
 SMALL_DIR = Path(__file__).parent.parent / "data" / "pics" / "covers_small"
 
 COVER_DIR.mkdir(parents=True, exist_ok=True)
 SMALL_DIR.mkdir(parents=True, exist_ok=True)
-
-def get_cover_source_id(song_id: int) -> int:
-    song_id = int(song_id)
-    if 10001 <= song_id <= 11000:
-        song_id -= 10000
-    return song_id
 
 async def getCover(song_id: int) -> Image.Image:
     file_path = COVER_DIR / f"{song_id % 10000:04d}.png"
@@ -26,7 +19,7 @@ async def getCover(song_id: int) -> Image.Image:
             os.remove(file_path)
         async with httpx.AsyncClient() as client:
             try:
-                resp = await client.get(COVER_URL.format(id=get_cover_source_id(song_id)))
+                resp = await client.get(COVER_URL.format(id=song_id % 10000))
                 if resp.status_code == 200:
                     with open(file_path, "wb") as f:
                         f.write(resp.content)
